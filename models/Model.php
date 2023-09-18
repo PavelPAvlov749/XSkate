9da5d4ca4b805ad753717ad32a4a05d4ff4eb532
PavelPAvlov749 <unklefraid@gmail.com>
2023-09-18 19:56:27 +0600

    sfe

--- models/Model.php
+++ models/Model.php
@@ -6,7 +6,7 @@ use PDOException;
 class Model {
     private string $hostname = "localhost";
     private string $login = "phpmyadmin";
-    private string $db_password = "*2DBD1C8A29DFA9FC25749849FE420D1E2EF8ED44";
+    private string $db_password = "2DBD1C8A29DFA9FC25749849FE420D1E2EF8ED44";
     private string $db_name = "XSkate";
     
     protected PDO $db;
